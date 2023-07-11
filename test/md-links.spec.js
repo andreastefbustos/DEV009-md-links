b39fbac683bcb53f344f5d2496cd2970aff69c9c
Andrea Bustos <andreastefbustos@gmail.com>
2023-07-11 12:04:31 -0400

    ADD tests

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,7 +1,9 @@
 const { mdLinks } = require('../index.js');
+const { validateURL } = require('../data.js');
 
 
 describe('mdLinks', () => {
+ 
   it('Should return a Promise', (done) => {
     mdLinks('test.md')
       .then((links) => {
@@ -12,4 +14,28 @@ describe('mdLinks', () => {
         done(error); // Llama a done(error) si ocurre un error durante la prueba
       });
   });
+
+  test('Should reject the promise if the path does not exist', () => {
+    expect.assertions(1); // Asegura que se realice al menos una afirmación dentro de la prueba
+  
+    const nonExistentPath = '/ruta/no/existente.md';
+  
+    return expect(mdLinks(nonExistentPath)).rejects.toThrow('La ruta no existe.');
+  });
 });
+
+describe('validateURL', () => {
+  test('Should return true if the URL is valid', () => {
+    const validURL = 'https://example.com';
+    expect(validateURL(validURL)).toBe(true);
+  });
+  
+  test('Should return true if the URL is not valid', () => {
+    const invalidURL = 'example.com';
+    expect(validateURL(invalidURL)).toBe(false);
+  });
+})
+
+
+
+
